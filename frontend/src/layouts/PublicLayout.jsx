1afed079285bb0ab460b9a39d2909ea9750640d0
Prashanth <protonater@live.com>
2023-11-30 14:38:12 -0800

    comments fix

--- frontend/src/layouts/PublicLayout.jsx
+++ frontend/src/layouts/PublicLayout.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import RequireAuth from 'components/RequireAuth';
-import * as appRoutes from 'constants/routes';
 
 const PublicLayout = () => {
   return (
-    <RequireAuth redirectTo={appRoutes.LOGIN}>
+    <>
       <main>
         <Outlet />
       </main>
-    </RequireAuth>
+    </>
   );
 };
 
