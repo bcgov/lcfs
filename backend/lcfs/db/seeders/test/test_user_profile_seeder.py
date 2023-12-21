11f10e19ac720d66fdd6f80eeaf7e8b838d26cae
Your Name <you@example.com>
2023-12-20 21:53:39 -0800

    feat: auth middleware pytests

--- backend/lcfs/db/seeders/test/test_user_profile_seeder.py
+++ backend/lcfs/db/seeders/test/test_user_profile_seeder.py
@@ -49,6 +49,54 @@ async def seed_test_user_profiles(session):
             "mobile_phone": fake.phone_number(),
             "organization_id": 3
         },
+        {
+            "keycloak_email": 'idir@test.com',
+            "keycloak_username": 'IDIRUSER',
+            "email": 'user@test.com',
+            "username": 'idiruser',
+            "display_name": fake.name(),
+            "title": "Developer",
+            "phone": fake.phone_number(),
+            "mobile_phone": fake.phone_number(),
+            "organization_id": 1,
+            "is_active": True
+        },
+        {
+            "keycloak_email": 'bceid@test.com',
+            "keycloak_username": 'BCEIDUSER',
+            "email": 'user@test.com',
+            "username": 'bceiduser',
+            "display_name": fake.name(),
+            "title": "Developer",
+            "phone": fake.phone_number(),
+            "mobile_phone": fake.phone_number(),
+            "organization_id": 2,
+            "is_active": True
+        },
+        {
+            "keycloak_email": 'active@test.com',
+            "keycloak_username": 'ACTIVEUSER',
+            "email": 'user@test.com',
+            "username": 'activeuser',
+            "display_name": fake.name(),
+            "title": "Developer",
+            "phone": fake.phone_number(),
+            "mobile_phone": fake.phone_number(),
+            "organization_id": 3,
+            "is_active": True
+        },
+        {
+            "keycloak_email": 'inactive@test.com',
+            "keycloak_username": 'INACTIVEUSER',
+            "email": 'user@test.com',
+            "username": 'inactiveuser',
+            "display_name": fake.name(),
+            "title": "Developer",
+            "phone": fake.phone_number(),
+            "mobile_phone": fake.phone_number(),
+            "organization_id": 3,
+            "is_active": False
+        },
     ]
 
     try:
