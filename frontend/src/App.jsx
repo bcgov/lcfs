6ca1a3795071fd90dacdc3d9acd9e87af5bbc290
Prashanth <protonater@live.com>
2024-01-19 10:02:44 -0800

    fix: revert

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { ROUTES } from './constants/routes'
 import { MainLayout } from './layouts/MainLayout'
 import { AddEditUser } from './views/AddEditUser'
 import { AddFuelCode } from './views/AddFuelCode'
-import { AddEditOrganization } from '@/views/AddOrganization/AddEditOrganization'
+import { AddOrganization } from './views/AddOrganization'
 import { AddTransaction } from './views/AddTransaction'
 import { AdminMenu } from './views/AdminMenu'
 import { ApiDocs } from './views/ApiDocs'
@@ -94,7 +94,7 @@ const router = createBrowserRouter([
       },
       {
         path: ROUTES.ORGANIZATIONS_ADD,
-        element: <AddEditOrganization />,
+        element: <AddOrganization />,
         handle: { title: 'Add Organization' }
       },
       {
@@ -104,7 +104,7 @@ const router = createBrowserRouter([
       },
       {
         path: ROUTES.ORGANIZATIONS_EDIT,
-        element: <AddEditOrganization />,
+        element: <AddOrganization />,
         handle: { title: 'Edit Organization' }
       },
       {
