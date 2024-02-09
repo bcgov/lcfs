21b6f91956799d20c562e093db3737814870ae2e
Hamed Valiollahi Bayeki <hamedv90@gmail.com>
2024-02-09 09:29:40 -0800

    feat: add seeding option to migration script

--- backend/migrate.sh
+++ backend/migrate.sh
@@ -3,9 +3,24 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 export PYTHONPATH=$SCRIPT_DIR:$PYTHONPATH
 
-# Installing dependencies using Poetry
-echo "Installing dependencies with Poetry..."
-poetry install
+echo '   __   _____________  ___  ___    __  ____               __  _         '
+echo '  / /  / ___/ __/ __/ / _ \/ _ )  /  |/  (_)__ ________ _/ /_(_)__  ___ '
+echo ' / /__/ /__/ _/_\ \  / // / _  | / /|_/ / / _ `/ __/ _ `/ __/ / _ \/ _ \'
+echo '/____/\___/_/ /___/ /____/____/ /_/  /_/_/\_, /_/  \_,_/\__/_/\___/_//_/'
+echo '                                         /___/                          '
+echo -e
+
+# Function to seed the database
+seed_database() {
+    echo "🌱  Seeding the database..."
+    if poetry run python /app/lcfs/db/seeders/seed_database.py; then
+        echo "✅  Database successfully seeded."
+    else
+        echo "❌  Seeding failed. Attempting to revert changes..."
+        downgrade_database
+        exit 1
+    fi
+}
 
 # Function for generating new migrations
 generate_migration() {
@@ -13,19 +28,34 @@ generate_migration() {
         echo "Usage: $0 -g <Description of changes>"
         exit 1
     fi
+    echo "🛠  Generating new migration: $1"
     poetry run alembic revision --autogenerate -m "$1"
+    echo "✅  New migration created."
 }
 
 # Function for upgrading the database
 upgrade_database() {
-    revision=${1:-head}
+    local revision=${1:-head}
+    echo "📈  Upgrading the database to revision: $revision"
     poetry run alembic upgrade $revision
+    echo "✅  Database upgraded to $revision."
 }
 
 # Function for downgrading the database
 downgrade_database() {
-    revision=${1:-base}
+    local revision=${1:-base}
+    echo "📉  Downgrading the database to revision: $revision"
     poetry run alembic downgrade $revision
+    echo "✅  Database downgraded to $revision."
+}
+
+# Function for resetting and seeding the database
+reset_and_seed_database() {
+    echo "🔄  Resetting and seeding the database..."
+    downgrade_database
+    upgrade_database
+    seed_database
+    echo "✅  Database reset and seeded. Ready for fresh start. 🌱"
 }
 
 # Function for displaying help manual
@@ -33,26 +63,37 @@ display_help() {
     echo "Usage: $0 [option]"
     echo "Options:"
     echo "  -g <message>  Generate new migration with a description."
-    echo "  -u [revision] Upgrade the database to a specific revision or to the latest ('head')."
-    echo "  -d [revision] Downgrade the database to a specific revision or to the base."
+    echo "  -u [revision] Upgrade the database to a specific revision or to the latest ('head'). Optional argument."
+    echo "  -d [revision] Downgrade the database to a specific revision or to the base. Optional argument."
+    echo "  -s            Reset and seed the database."
     echo "  -h            Display this help manual."
 }
 
+# Installing dependencies using Poetry
+echo "📚  Installing dependencies with Poetry..."
+poetry install
+echo "✅  Dependencies installed."
+echo "====================================================="
+
+# Check if no arguments were provided
+if [ $# -eq 0 ]; then
+    display_help
+    exit 0
+fi
+
 # Command line options
-while getopts ":g:u::d::h" opt; do
+while getopts ":g:u::d::sh" opt; do
     case $opt in
         g) generate_migration "$OPTARG" ;;
         u) upgrade_database "$OPTARG" ;;
         d) downgrade_database "$OPTARG" ;;
+        s) reset_and_seed_database ;;
         h) display_help; exit 0 ;;
-        \?) echo "Invalid option -$OPTARG" >&2; exit 1 ;;
-        :) if [ "$OPTARG" = "u" ]; then
-              upgrade_database
-           elif [ "$OPTARG" = "d" ]; then
-              downgrade_database
-           else
-              echo "Option -$OPTARG requires an argument." >&2
-              exit 1
-           fi ;;
+        \?) echo "❌  Invalid option -$OPTARG" >&2; exit 1 ;;
+        :) case $OPTARG in
+                u) upgrade_database ;;
+                d) downgrade_database ;;
+                *) echo "❌  Option -$OPTARG requires an argument." >&2; exit 1 ;;
+           esac ;;
     esac
 done
