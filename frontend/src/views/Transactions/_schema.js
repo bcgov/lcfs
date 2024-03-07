e33ba392a214c0202b50b8b0406e81c3b81861bc
Prashanth <protonater@live.com>
2024-03-07 02:16:30 -0800

    fix: .

--- frontend/src/views/Transactions/_schema.js
+++ frontend/src/views/Transactions/_schema.js
@@ -4,23 +4,23 @@ import BCColumnSetFilter from '@/components/BCDataGrid/BCColumnSetFilter'
 import { useTransactionStatuses } from '@/hooks/useTransactions'
 
 export const transactionsColDefs = (t) => [
-  { colId: 'transaction_id', field: 'transaction_id', headerName: t('txn:txnColLabels.txnId'), maxWidth: 800, flex: 1 },
-  { colId: 'transaction_type', field: 'transaction_type', headerName: t('txn:txnColLabels.type'), flex: 1 },
-  { colId: 'from_organization', field: 'from_organization', headerName: t('txn:txnColLabels.organizationFrom'), flex: 1 },
-  { colId: 'to_organization', field: 'to_organization', headerName: t('txn:txnColLabels.organizationTo'), flex: 1 },
+  { colId: 'transaction_id', field: 'transaction_id', headerName: t('txn:txnColLabels.txnId'), maxWidth: 120 },
+  { colId: 'transaction_type', field: 'transaction_type', headerName: t('txn:txnColLabels.type'), maxWidth: 150 },
+  { colId: 'from_organization', field: 'from_organization', headerName: t('txn:txnColLabels.organizationFrom'), flex: 2 },
+  { colId: 'to_organization', field: 'to_organization', headerName: t('txn:txnColLabels.organizationTo'), flex: 2 },
   {
     colId: 'quantity',
     field: 'quantity',
     headerName: t('txn:txnColLabels.quantity'),
     valueFormatter: numberFormatter,
-    flex: 1
+    maxWidth: 190,
   },
   {
     colId: 'price_per_unit',
     field: 'price_per_unit',
     headerName: t('txn:txnColLabels.pricePerUnit'),
     valueFormatter: currencyFormatter,
-    flex: 1
+    maxWidth: 190,
   },
   {
     colId: 'status',
@@ -37,14 +37,14 @@ export const transactionsColDefs = (t) => [
       multiple: false
     },
     suppressMenu: true,
-    flex: 1
+    maxWidth: 190
   },
   {
     colId: 'update_date',
     field: 'update_date',
     headerName: t('txn:txnColLabels.updateDate'),
     valueFormatter: dateFormatter,
-    flex: 1
+    maxWidth: 180,
   },
 ];
 
