e7ef45b38c83cc3eaf635782a31c954ddb635340
prv-proton <prashanth.venkateshappa@gov.bc.ca>
2024-05-22 03:12:33 -0700

    demo

--- frontend/waitforconnection.sh
+++ frontend/waitforconnection.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# max_attempts=3
-# wait_time=15
-# url=https://lcfs-dev.apps.silver.devops.gov.bc.ca/login
-# code=200
+max_attempts=3
+wait_time=15
+url=https://lcfs-dev.apps.silver.devops.gov.bc.ca/login
+code=200
 if [ $(curl --output /dev/null --silent --head -X GET --retry ${max_attempts} --fail --retry-all-errors --retry-delay ${wait_time} --retry-max-time 240 -w "%{response_code}\n" ${url}) -eq ${code} ]; then
 	echo "Connection Success!"
 else
