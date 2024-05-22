5461a0ae8e7f0cc4fe2a55351b5532e9a068ada0
prv-proton <prashanth.venkateshappa@gov.bc.ca>
2024-05-22 06:05:28 -0700

    test

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
     viewportHeight: 720,
 
     // Base URL for tests
-    baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:3000',
+    // baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:3000',
     async setupNodeEvents(on, config) {
       // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
       await addCucumberPreprocessorPlugin(on, config);
