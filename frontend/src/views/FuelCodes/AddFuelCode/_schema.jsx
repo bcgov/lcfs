340e67deaa1fe41471a1fe393140e17f1a48c5b0
kevin-hashimoto <kevin.hashimoto@gov.bc.ca>
2024-06-04 16:37:12 -0700

    chore: remove comment

--- frontend/src/views/FuelCodes/AddFuelCode/_schema.jsx
+++ frontend/src/views/FuelCodes/AddFuelCode/_schema.jsx
@@ -291,22 +291,6 @@ export const fuelCodeColDefs = (t, optionsData) => [
     },
     cellDataType: 'text'
   },
-  // {
-  //   field: 'lastUpdated',
-  //   headerName: t('fuelCode:fuelCodeColLabels.lastUpdated'),
-  //   maxWidth: 180,
-  //   minWidth: 180,
-  //   cellRenderer: (params) => (
-  //     <Typography variant="body4">
-  //       {params.value ? params.value : 'YYYY-MM-DD'}
-  //     </Typography>
-  //   ),
-  //   editable: false, // TODO: change as per #516
-  //   cellDataType: 'dateString',
-  //   // valueGetter: (params) => {
-  //   //   return new Date().toLocaleDateString()
-  //   // }
-  // },
   {
     field: 'applicationDate',
     headerName: t('fuelCode:fuelCodeColLabels.applicationDate'),
