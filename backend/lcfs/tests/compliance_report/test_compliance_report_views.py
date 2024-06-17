4a010351416c46fdc2187b54e55492f6d309fb53
prv-proton <prashanth.venkateshappa@gov.bc.ca>
2024-06-17 07:29:45 -0700

    update py tests

--- backend/lcfs/tests/compliance_report/test_compliance_report_views.py
+++ backend/lcfs/tests/compliance_report/test_compliance_report_views.py
@@ -62,20 +62,21 @@ async def test_get_compliance_report_by_id_for_bceid_user(
     client: AsyncClient, fastapi_app: FastAPI, set_mock_user_roles
 ) -> None:
     set_mock_user_roles(fastapi_app, ["Supplier"])
+    try:
+        with open(Path(__file__).parent / "report_payload.json") as f_data:
+            payload = json.load(f_data)
+            url = fastapi_app.url_path_for(
+                "create_compliance_report", organization_id=1
+            )
+            response = await client.post(url, json=payload["draftReport"])
+            assert response.status_code == status.HTTP_201_CREATED
+            assert response.content
+            f_data.close()
+    except Exception as e:
+        raise AssertionError("Failed to test get compliance report by ID:", e)
+
     url = fastapi_app.url_path_for(
         "get_compliance_report_by_id", organization_id=1, report_id=1
-    )
-    response = await client.get(url)
-    assert response.status_code == status.HTTP_200_OK
-    assert response.content
-
-@pytest.mark.anyio
-async def test_get_compliance_report_by_id_for_idir_user(
-    client: AsyncClient, fastapi_app: FastAPI, set_mock_user_roles
-) -> None:
-    set_mock_user_roles(fastapi_app, ["Government"])
-    url = fastapi_app.url_path_for(
-        "get_compliance_report_by_id", report_id=1
     )
     response = await client.get(url)
     assert response.status_code == status.HTTP_200_OK
