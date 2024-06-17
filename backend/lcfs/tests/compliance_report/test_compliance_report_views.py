651e94a4ee384f69fdb0b84de63ba872471498be
prv-proton <prashanth.venkateshappa@gov.bc.ca>
2024-06-17 08:00:13 -0700

    .

--- backend/lcfs/tests/compliance_report/test_compliance_report_views.py
+++ backend/lcfs/tests/compliance_report/test_compliance_report_views.py
@@ -43,18 +43,11 @@ async def test_create_compliance_report_draft(
     client: AsyncClient, fastapi_app: FastAPI, set_mock_user_roles
 ) -> None:
     set_mock_user_roles(fastapi_app, ["Supplier"])
-    try:
-        with open(Path(__file__).parent / "report_payload.json") as f_data:
-            payload = json.load(f_data)
-            url = fastapi_app.url_path_for(
-                "create_compliance_report", organization_id=1
-            )
-            response = await client.post(url, json=payload["draftReport"])
-            assert response.status_code == status.HTTP_201_CREATED
-            assert response.content
-            f_data.close()
-    except Exception as e:
-        raise AssertionError("Failed to create compliance report", e)
+    payload = {"compliancePeriod": "2024", "organizationId": 1, "status": "Draft"}
+    url = fastapi_app.url_path_for("create_compliance_report", organization_id=1)
+    response = await client.post(url, json=payload)
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.content
 
 
 @pytest.mark.anyio
@@ -62,21 +55,17 @@ async def test_get_compliance_report_by_id_for_bceid_user(
     client: AsyncClient, fastapi_app: FastAPI, set_mock_user_roles
 ) -> None:
     set_mock_user_roles(fastapi_app, ["Supplier"])
-    try:
-        with open(Path(__file__).parent / "report_payload.json") as f_data:
-            payload = json.load(f_data)
-            url = fastapi_app.url_path_for(
-                "create_compliance_report", organization_id=1
-            )
-            response = await client.post(url, json=payload["draftReport"])
-            assert response.status_code == status.HTTP_201_CREATED
-            assert response.content
-            f_data.close()
-    except Exception as e:
-        raise AssertionError("Failed to test get compliance report by ID:", e)
-
+    payload = {"compliancePeriod": "2023", "organizationId": 1, "status": "Draft"}
+    url = fastapi_app.url_path_for("create_compliance_report", organization_id=1)
+    response = await client.post(url, json=payload)
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.content
+    compliance_report_id = json.loads(response.content.decode("utf-8"))[
+        "complianceReportId"
+    ]
+    # test to get the the report back after creating
     url = fastapi_app.url_path_for(
-        "get_compliance_report_by_id", organization_id=1, report_id=1
+        "get_compliance_report_by_id", organization_id=1, report_id=compliance_report_id
     )
     response = await client.get(url)
     assert response.status_code == status.HTTP_200_OK
