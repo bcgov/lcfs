2610621db870d6b6a2c0f3dd867c394b78b9add8
prv-proton <prashanth.venkateshappa@gov.bc.ca>
2024-07-24 12:58:23 -0700

    remove redundant workflows

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -21,12 +21,25 @@ Cypress.Commands.add('loginWith', (userType, username, password) => {
   cy.getByDataTest(userType === 'idir' ? 'link-idir' : 'link-bceid').click()
   cy.wait(5000)
   // Define the login process for IDIR and BCeID
-  cy.get("#user").type(username, { log: false });
-  cy.get("#password").type(password, { log: false });
-  cy.get("div.login-form-action > input").click();
-  cy.wait(5000)
+  // cy.get("#user").type(username, { log: false });
+  // cy.get("#password").type(password, { log: false });
+  // cy.get("div.login-form-action > input").click();
+  // cy.wait(5000)
+  const loginProcess = (args) => {
+    const [username, password] = args
+    cy.get('input[name=user]').type(username, { log: false })
+    cy.get('input[name=password]').type(password, { log: false })
+    cy.get('form').submit()
+  }
+
+  // Perform login on the appropriate page
+  cy.origin(
+    'https://logontest7.gov.bc.ca',
+    { args: [username, password] },
+    loginProcess
+  )
   // Check to confirm successful login
-  cy.getByDataTest('logout-button').should('be.visible')
+  // cy.getByDataTest('logout-button').should('be.visible')
 })
 
 /**
