47f58b20105f9ebae709da9d55aacf1030be69c6
prv-proton <prashanth.venkateshappa@gov.bc.ca>
2024-09-04 06:55:42 -0700

    .

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -4,26 +4,6 @@ import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-prepro
 import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
 import { GenerateCtrfReport } from 'cypress-ctrf-json-reporter'
 
-function initPlugins(on, plugins) {
-  const eventCallbacks = {}
-
-  const customOn = (eventName, callback) => {
-    if (!eventCallbacks[eventName]) {
-      eventCallbacks[eventName] = []
-      // Register a single handler for each event that will execute all registered callbacks
-      on(eventName, async (...args) => {
-        for (const cb of eventCallbacks[eventName]) {
-          await cb(...args)
-        }
-      })
-    }
-    eventCallbacks[eventName].push(callback)
-  }
-
-  // Initialize each plugin with the custom `on` handler
-  plugins.forEach((plugin) => plugin(customOn))
-}
-
 export default defineConfig({
   e2e: {
     specPattern: ["**/*.feature", "**/*.cy.js"],
@@ -55,23 +35,19 @@ export default defineConfig({
     async setupNodeEvents(on, config) {
       // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
       await addCucumberPreprocessorPlugin(on, config);
-      initPlugins(on, [(on) => new GenerateCtrfReport({ on })])
 
-      on(
-        "file:preprocessor",
-        createBundler({
-          plugins: [createEsbuildPlugin(config)],
-        })
-      );
+      on("file:preprocessor", createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      }));
 
-      // on('after:run', async (results) => {
-      //   const ctrfReporter =  new GenerateCtrfReport({
-      //     outputDir: "cypress/reports/",
-      //     outputFile: "cypress/reports/ctrf-report.json",
-      //     on: results
-      //   });
-      //   ctrfReporter.generateCtrfReport();
-      // });
+      // Add GenerateCtrfReport to the on('after:run') event
+      on('after:run', async (results) => {
+        const ctrfReport = new GenerateCtrfReport({
+          jsonDir: "cypress/reports",
+          outputFile: "cypress/reports/ctrf-report.json"
+        });
+        await ctrfReport.generate();
+      });
 
       return config;
     },
