80c5cc9073a1175914d4ed7118c167a96768f09d
Arturo Reyes Lopez <arturo_reyes_lopez@hotmail.com>
2024-11-01 16:38:53 -0600

    Make to work auditable fields in audit_log table

--- backend/lcfs/db/dependencies.py
+++ backend/lcfs/db/dependencies.py
@@ -3,10 +3,11 @@ from typing import AsyncGenerator
 
 from fastapi import Request
 from redis import asyncio as aioredis
+from sqlalchemy import text
 from sqlalchemy.engine import make_url
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
-from lcfs.db.base import current_user_var
+from lcfs.db.base import current_user_var, get_current_user
 from lcfs.settings import settings
 
 if settings.environment == "dev":
@@ -15,6 +16,19 @@ if settings.environment == "dev":
 db_url = make_url(str(settings.db_url.with_path(f"/{settings.db_base}")))
 async_engine = create_async_engine(db_url, future=True)
 logging.getLogger("sqlalchemy.engine").setLevel(logging.WARN)
+
+async def set_user_context(session: AsyncSession, username: str):
+    """
+    Set user_id context for the session to be used in auditing.
+    """
+    try:
+        await session.execute(text(f"SET SESSION app.username = '{username}'"))
+
+        logging.info(f"SET SESSION app.username = '{username}' executed successfully")
+
+    except Exception as e:
+        logging.error(f"Failed to execute SET LOCAL app.user_id = '{username}': {e}")
+        raise e
 
 
 async def get_async_db_session(request: Request) -> AsyncGenerator[AsyncSession, None]:
@@ -26,6 +40,8 @@ async def get_async_db_session(request: Request) -> AsyncGenerator[AsyncSession,
         async with session.begin():
             if request.user:
                 current_user_var.set(request.user)
+                current_user = get_current_user()
+                await set_user_context(session, current_user)
             try:
                 yield session
                 await session.flush()
--- backend/lcfs/db/models/audit/AuditLog.py
+++ backend/lcfs/db/models/audit/AuditLog.py
@@ -1,23 +1,15 @@
 from lcfs.db.base import Auditable, BaseModel
 from sqlalchemy import (
-    TIMESTAMP,
     BigInteger,
     Column,
-    Integer,
-    String,
-    Boolean,
-    ForeignKey,
     Text,
-    UniqueConstraint,
-    func,
 )
-from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import JSONB
 
 
 class AuditLog(BaseModel, Auditable):
     __tablename__ = "audit_log"
-    __table_args__ = {"comment": "Tracks changes in defined tables."}
+    __table_args__ = {"comment": "Track changes in defined tables."}
 
     id = Column(BigInteger, primary_key=True, autoincrement=True)
 
