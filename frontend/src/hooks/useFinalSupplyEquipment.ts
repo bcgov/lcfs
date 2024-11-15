bc37b4146d682569137c16992b8dec55e815f611
kevin-hashimoto <kevin.hashimoto@gov.bc.ca>
2024-11-14 17:30:51 -0800

    refactor: useFinalSupplyEquipment hook to ts

---
+++ frontend/src/hooks/useFinalSupplyEquipment.ts
@@ -0,0 +1,71 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+
+import {
+  FinalSupplyEquipmentCreateSchema,
+  FinalSupplyEquipmentsService
+} from '@/services/apiClient'
+
+export const useFinalSupplyEquipmentOptions = () => {
+  return useQuery({
+    queryKey: ['final-supply-equipment-options'],
+    queryFn: async () => {
+      try {
+        const { data } = await FinalSupplyEquipmentsService.getFseOptions()
+
+        return data
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  })
+}
+
+export const useGetFinalSupplyEquipments = (complianceReportId: string) => {
+  return useQuery({
+    queryKey: ['final-supply-equipments', complianceReportId],
+    queryFn: async () => {
+      try {
+        const { data } =
+          await FinalSupplyEquipmentsService.getFinalSupplyEquipments({
+            body: {
+              complianceReportId: +complianceReportId
+            }
+          })
+        return data
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  })
+}
+
+export const useSaveFinalSupplyEquipment = (complianceReportId: string) => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: async (data: FinalSupplyEquipmentCreateSchema) => {
+      const body = {
+        ...data,
+        levelOfEquipment: data.levelOfEquipment,
+        fuelMeasurementType: data.fuelMeasurementType,
+        complianceReportId: +complianceReportId
+      }
+
+      try {
+        return await FinalSupplyEquipmentsService.saveFinalSupplyEquipmentRow({
+          body
+        })
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['final-supply-equipments', complianceReportId]
+      })
+      queryClient.invalidateQueries({
+        queryKey: ['compliance-report-summary', complianceReportId]
+      })
+    }
+  })
+}
