b292072cb2317fab1ececeb0ff6f6c6906673b58
kevin-hashimoto <kevin.hashimoto@gov.bc.ca>
2024-11-20 10:42:40 -0800

    fix: alembic head?

---
+++ backend/lcfs/db/migrations/versions/2024-11-20-18-41_65532f1f67e6.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 65532f1f67e6
+Revises: 654858661ae7, 1974af823b80
+Create Date: 2024-11-20 18:41:16.482524
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "65532f1f67e6"
+down_revision = ("654858661ae7", "1974af823b80")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
