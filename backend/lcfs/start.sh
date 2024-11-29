e143eea504e4a1b23e38ccfd466017e1e75ed3d9
Kuan Fan <kuan.fan@gov.bc.ca>
2024-11-29 15:09:03 -0800

    update start.sh

--- backend/lcfs/start.sh
+++ backend/lcfs/start.sh
@@ -11,6 +11,6 @@ if [ "$APP_ENVIRONMENT" = "prod" ]; then
     echo "Prod build"
     uvicorn main:lcfs --host 0.0.0.0 --port 8000 --workers 2
 else
-    cho "Non-prod build"
+    echo "Non-prod build"
     poetry run python -m lcfs
-
+    
