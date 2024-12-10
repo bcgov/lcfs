c8afc9e9c7c097717ec1a5b9dd25ca0943acf28a
prv-proton <prashanth.venkateshappa@gov.bc.ca>
2024-12-10 11:01:00 -0800

    complete

--- etl/nifi_scripts/adminAdjTrxn.groovy
+++ etl/nifi_scripts/adminAdjTrxn.groovy
@@ -6,7 +6,7 @@ import java.time.OffsetDateTime
 import java.sql.Timestamp
 
 def SOURCE_QUERY = """
-          WITH
+WITH
         internal_comment AS (
           SELECT
             ctc.id,
@@ -33,24 +33,15 @@ def SOURCE_QUERY = """
             ctc.create_timestamp
         )
       SELECT
-        ct.id AS transfer_id,
-        ct.initiator_id AS from_organization_id,
+        ct.id AS admin_adjustment_id,
         ct.respondent_id AS to_organization_id,
         ct.date_of_written_agreement AS agreement_date,
         ct.trade_effective_date AS transaction_effective_date,
-        ct.fair_market_value_per_credit AS price_per_unit,
-        ct.number_of_credits AS quantity,
+        ct.number_of_credits AS compliance_units,
         ct.create_user_id as create_user,
         ct.update_user_id as update_user,
         ct.update_timestamp as update_date,
         ct.create_timestamp  as create_date,
-        -- Aggregate comments from initiator's organization
-        COALESCE(
-          ctzr.description,
-          STRING_AGG (DISTINCT from_ctc.credit_trade_comment, '; ')
-        ) AS from_org_comment,
-        -- Aggregate comments from respondent's organization
-        STRING_AGG (DISTINCT to_ctc.credit_trade_comment, '; ') AS to_org_comment,
         -- Aggregate comments from government with internal comment handling
         STRING_AGG (DISTINCT gov_ctc.credit_trade_comment, '; ') AS gov_comment,
         -- JSON aggregation for internal comments
@@ -58,18 +49,14 @@ def SOURCE_QUERY = """
         -- JSON aggregation for credit trade history
         json_agg (
           json_build_object (
-            'transfer_id',
+            'admin_adjustment_id',
             cth.credit_trade_id,
-            'transfer_status',
+            'admin_adjustment_status',
             case
-              WHEN cts_history.status = 'Cancelled' or cth.is_rescinded = true THEN 'Rescinded'
-              WHEN cts_history.status = 'Accepted' THEN 'Submitted'
-              WHEN cts_history.status IN ('Approved', 'Recorded') THEN 'Recorded'
-              WHEN cts_history.status IN ('Not Recommended', 'Declined') THEN 'Refused'
-              WHEN cts_history.status = 'Declined' THEN 'Refused'
-              WHEN cts_history.status = 'Refused' THEN 'Declined'
-              WHEN cts_history.status = 'Submitted' AND cth.is_rescinded = false THEN 'Sent'
-              ELSE cts_history.status
+              WHEN cts_history.status IN ('Cancelled', 'Not Recommended', 'Declined', 'Refused') or ct.is_rescinded = true THEN 'Deleted'
+              WHEN cts_history.status IN ('Accepted', 'Submitted', 'Recommended') THEN 'Recommended'
+              WHEN cts_history.status IN ('Approved', 'Recorded') THEN 'Approved'
+              ELSE 'Draft'
             END,
             'user_profile_id',
             cth.create_user_id,
@@ -77,22 +64,12 @@ def SOURCE_QUERY = """
             cth.create_timestamp
           )
         ) AS credit_trade_history,
-        COALESCE(ctc.category, NULL) AS transfer_category,
         case
-          WHEN cts.status = 'Cancelled' or ct.is_rescinded = true THEN 'Rescinded'
-          WHEN cts.status = 'Accepted' THEN 'Submitted'
-          WHEN cts.status IN ('Approved', 'Recorded') THEN 'Recorded'
-          WHEN cts.status IN ('Not Recommended', 'Declined') THEN 'Refused'
-          WHEN cts.status = 'Declined' THEN 'Refused'
-          WHEN cts.status = 'Refused' THEN 'Declined'
-          WHEN cts.status = 'Submitted' THEN 'Sent'
-          ELSE cts.status
-        END AS current_status,
-        CASE
-          WHEN cts.status = 'Not Recommended' THEN 'Refuse'
-          WHEN cts.status = 'Recommended' THEN 'Record'
-          ELSE NULL
-        END AS recommendation
+          WHEN cts.status IN ('Cancelled', 'Not Recommended', 'Declined', 'Refused') or ct.is_rescinded = true THEN 'Deleted'
+          WHEN cts.status IN ('Accepted', 'Submitted', 'Recommended') THEN 'Recommended'
+          WHEN cts.status IN ('Approved', 'Recorded') THEN 'Approved'
+          ELSE 'Draft'
+        END AS current_status, cts.status
       FROM
         credit_trade ct
         JOIN credit_trade_type ctt ON ct.type_id = ctt.id
@@ -137,16 +114,13 @@ def SOURCE_QUERY = """
         LEFT JOIN credit_trade_history cth ON cth.credit_trade_id = ct.id
         JOIN credit_trade_status cts_history ON cth.status_id = cts_history.id
       WHERE
-        ctt.the_type IN ('Buy', 'Sell')
+        ctt.the_type IN ('Administrative Adjustment')
       GROUP BY
         ct.id,
-        ct.initiator_id,
         ct.respondent_id,
         ct.date_of_written_agreement,
         ct.trade_effective_date,
-        ct.fair_market_value_per_credit,
         ct.number_of_credits,
-        ctc.category,
         cts.status,
         ctzr.description,
         internal_comment.role_names;
@@ -193,19 +167,18 @@ try {
         def internalCommentsJson = internalComments ? jsonSlurper.parseText(internalComments) : []
         def creditTradeHistoryJson = creditTradeHistory ? jsonSlurper.parseText(creditTradeHistory) : []
 
-        def (fromTransactionId, toTransactionId) = processTransactions(resultSet.getString('current_status'),
-                resultSet,
-                statements.transactionStmt)
+        def toTransactionId = processTransactions(resultSet.getString('current_status'),
+                resultSet, statements.transactionStmt)
 
-        def transferId = insertTransfer(resultSet, statements.transferStmt,
-                fromTransactionId, toTransactionId, preparedData, destinationConn)
+        def adminAdjustmentId = insertadminAdjustment(resultSet, statements.adminAdjustmentStmt,
+                toTransactionId, preparedData, destinationConn)
 
-        if (transferId) {
-            processHistory(transferId, creditTradeHistoryJson, statements.historyStmt, preparedData)
-            processInternalComments(transferId, internalCommentsJson, statements.internalCommentStmt,
-                    statements.transferInternalCommentStmt)
+        if (adminAdjustmentId) {
+            processHistory(adminAdjustmentId, creditTradeHistoryJson, statements.historyStmt, preparedData)
+            processInternalComments(adminAdjustmentId, internalCommentsJson, statements.internalCommentStmt,
+                    statements.adminAdjustmentInternalCommentStmt)
         } else {
-            log.warn("Transfer not inserted for record: ${resultSet.getInt('transfer_id')}")
+            log.warn("admin-adjustment not inserted for record: ${resultSet.getInt('admin_adjustment_id')}")
         }
     }
     resultSet.close()
@@ -263,24 +236,14 @@ def loadTableData(Connection conn, String query, String keyColumn, String valueC
 }
 
 def prepareData(Connection conn) {
-    def categoryMap = loadTableData(conn,
-        'SELECT DISTINCT category, MIN(transfer_category_id) AS transfer_category_id FROM transfer_category GROUP BY category',
-        'category',
-        'transfer_category_id'
-    )
     def statusMap = loadTableData(conn,
-        'SELECT DISTINCT status, MIN(transfer_status_id) AS transfer_status_id FROM transfer_status GROUP BY status',
+        'SELECT DISTINCT status, MIN(admin_adjustment_status_id) AS admin_adjustment_status_id FROM admin_adjustment_status GROUP BY status',
         'status',
-        'transfer_status_id'
+        'admin_adjustment_status_id'
     )
     return [
-            categoryMap: categoryMap,
             statusMap  : statusMap
     ]
-}
-
-def getTransferCategoryId(String category, Map preparedData) {
-    return preparedData.categoryMap[category]
 }
 
 def getStatusId(String status, Map preparedData) {
@@ -288,17 +251,17 @@ def getStatusId(String status, Map preparedData) {
 }
 
 def prepareStatements(Connection conn) {
-    def INSERT_TRANSFER_SQL = '''
-          INSERT INTO transfer (
-              from_organization_id, to_organization_id, from_transaction_id, to_transaction_id,
-              agreement_date, transaction_effective_date, price_per_unit, quantity, from_org_comment, to_org_comment, gov_comment,
-              transfer_category_id, current_status_id, recommendation, create_date, update_date, create_user, update_user, effective_status, transfer_id
-          ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?::transfer_recommendation_enum, ?, ?, ?, ?, true, ?)
-          RETURNING transfer_id
+    def INSERT_admin_adjustment_SQL = '''
+          INSERT INTO admin_adjustment (
+              to_organization_id, transaction_id, transaction_effective_date, compliance_units, gov_comment,
+              current_status_id, create_date, update_date, create_user, update_user, effective_status,
+              admin_adjustment_id
+          ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, true, ?)
+          RETURNING admin_adjustment_id
       '''
-    def INSERT_TRANSFER_HISTORY_SQL = '''
-          INSERT INTO transfer_history (
-              transfer_history_id, transfer_id, transfer_status_id, user_profile_id, create_date, effective_status
+    def INSERT_admin_adjustment_HISTORY_SQL = '''
+          INSERT INTO admin_adjustment_history (
+              admin_adjustment_history_id, admin_adjustment_id, admin_adjustment_status_id, user_profile_id, create_date, effective_status
           ) VALUES (DEFAULT, ?, ?, ?, ?, true)
       '''
     def INSERT_INTERNAL_COMMENT_SQL = '''
@@ -307,9 +270,9 @@ def prepareStatements(Connection conn) {
           ) VALUES (DEFAULT, ?, ?::audience_scope, ?, ?)
           RETURNING internal_comment_id
       '''
-    def INSERT_TRANSFER_INTERNAL_COMMENT_SQL = '''
-          INSERT INTO transfer_internal_comment (
-              transfer_id, internal_comment_id
+    def INSERT_admin_adjustment_INTERNAL_COMMENT_SQL = '''
+          INSERT INTO admin_adjustment_internal_comment (
+              admin_adjustment_id, internal_comment_id
           ) VALUES (?, ?)
       '''
     def INSERT_TRANSACTION_SQL = '''
@@ -318,10 +281,11 @@ def prepareStatements(Connection conn) {
           ) VALUES (DEFAULT, ?, ?, ?::transaction_action_enum, ?, ?, ?, true)
           RETURNING transaction_id
       '''
-    return [transferStmt               : conn.prepareStatement(INSERT_TRANSFER_SQL),
-            historyStmt                : conn.prepareStatement(INSERT_TRANSFER_HISTORY_SQL),
+    return [adminAdjustmentStmt    : conn.prepareStatement(INSERT_admin_adjustment_SQL),
+            historyStmt                : conn.prepareStatement(INSERT_admin_adjustment_HISTORY_SQL),
             internalCommentStmt        : conn.prepareStatement(INSERT_INTERNAL_COMMENT_SQL),
-            transferInternalCommentStmt: conn.prepareStatement(INSERT_TRANSFER_INTERNAL_COMMENT_SQL),
+            adminAdjustmentInternalCommentStmt
+                    : conn.prepareStatement(INSERT_admin_adjustment_INTERNAL_COMMENT_SQL),
             transactionStmt            : conn.prepareStatement(INSERT_TRANSACTION_SQL)]
 }
 
@@ -337,26 +301,17 @@ def toSqlTimestamp(String timestampString) {
 }
 
 def processTransactions(String currentStatus, ResultSet rs, PreparedStatement stmt) {
-    def fromTransactionId = null
     def toTransactionId = null
 
-    switch (currentStatus) {
-        case ['Draft', 'Deleted', 'Refused', 'Declined', 'Rescinded']:
-            break
-        case ['Sent', 'Submitted', 'Recommended']:
-            fromTransactionId = insertTransaction(stmt, rs, 'Reserved', rs.getInt('from_organization_id'))
-            break
-        case 'Recorded':
-            fromTransactionId = insertTransaction(stmt, rs, 'Adjustment', rs.getInt('from_organization_id'))
-            toTransactionId = insertTransaction(stmt, rs, 'Adjustment', rs.getInt('to_organization_id'))
-            break
+    if (currentStatus == 'Approved') {
+      toTransactionId = insertTransaction(stmt, rs, 'Adjustment', rs.getInt('to_organization_id'))
     }
 
-    return [fromTransactionId, toTransactionId]
+    return toTransactionId
 }
 
 def insertTransaction(PreparedStatement stmt, ResultSet rs, String action, int orgId) {
-    stmt.setInt(1, rs.getInt('quantity'))
+    stmt.setInt(1, rs.getInt('compliance_units'))
     stmt.setInt(2, orgId)
     stmt.setString(3, action)
     stmt.setDate(4, rs.getDate('transaction_effective_date') ?: rs.getDate('agreement_date'))
@@ -367,21 +322,21 @@ def insertTransaction(PreparedStatement stmt, ResultSet rs, String action, int o
     return result.next() ? result.getInt('transaction_id') : null
 }
 
-def processHistory(Integer transferId, List creditTradeHistory, PreparedStatement historyStmt, Map preparedData) {
+def processHistory(Integer adminAdjustmentId, List creditTradeHistory, PreparedStatement historyStmt, Map preparedData) {
     if (!creditTradeHistory) return
 
-    // Use a Set to track unique combinations of transfer_id and transfer_status
+    // Use a Set to track unique combinations of admin_adjustment_id and admin_adjustment_status
     def processedEntries = new HashSet<String>()
 
     creditTradeHistory.each { historyItem ->
         try {
-            def statusId = getStatusId(historyItem.transfer_status, preparedData)
-            def uniqueKey = "${transferId}_${statusId}"
+            def statusId = getStatusId(historyItem.admin_adjustment_status, preparedData)
+            def uniqueKey = "${adminAdjustmentId}_${statusId}"
 
             // Check if this combination has already been processed
             if (!processedEntries.contains(uniqueKey)) {
                 // If not processed, add to batch and mark as processed
-                historyStmt.setInt(1, transferId)
+                historyStmt.setInt(1, adminAdjustmentId)
                 historyStmt.setInt(2, statusId)
                 historyStmt.setInt(3, historyItem.user_profile_id)
                 historyStmt.setTimestamp(4, toSqlTimestamp(historyItem.create_timestamp ?: '2013-01-01T00:00:00Z'))
@@ -390,7 +345,7 @@ def processHistory(Integer transferId, List creditTradeHistory, PreparedStatemen
                 processedEntries.add(uniqueKey)
             }
         } catch (Exception e) {
-            log.error("Error processing history record for transfer_id: ${transferId}", e)
+            log.error("Error processing history record for admin_adjustment_id: ${adminAdjustmentId}", e)
         }
     }
 
@@ -399,9 +354,9 @@ def processHistory(Integer transferId, List creditTradeHistory, PreparedStatemen
 }
 
 
-def processInternalComments(Integer transferId, List internalComments,
+def processInternalComments(Integer adminAdjustmentId, List internalComments,
                             PreparedStatement internalCommentStmt,
-                            PreparedStatement transferInternalCommentStmt) {
+                            PreparedStatement adminAdjustmentInternalCommentStmt) {
     if (!internalComments) return
 
     internalComments.each { comment ->
@@ -419,15 +374,15 @@ def processInternalComments(Integer transferId, List internalComments,
             if (commentResult.next()) {
                 internalCommentId = commentResult.getInt('internal_comment_id')
 
-                // Insert the transfer-comment relationship
-                transferInternalCommentStmt.setInt(1, transferId)
-                transferInternalCommentStmt.setInt(2, internalCommentId)
-                transferInternalCommentStmt.executeUpdate()
-            }
+                // Insert the admin-adjustment-comment relationship
+                adminAdjustmentInternalCommentStmt.setInt(1, adminAdjustmentId)
+                adminAdjustmentInternalCommentStmt.setInt(2, internalCommentId)
+                adminAdjustmentInternalCommentStmt.executeUpdate()
+            } 
 
             commentResult.close()
         } catch (Exception e) {
-            log.error("Error processing internal comment for transfer ${transferId}: ${e.getMessage()}", e)
+            log.error("Error processing internal comment for admin-adjustment ${adminAdjustmentId}: ${e.getMessage()}", e)
         }
     }
                             }
@@ -446,12 +401,12 @@ def getAudienceScope(String roleNames) {
     }
 }
 
-def insertTransfer(ResultSet rs, PreparedStatement transferStmt, Long fromTransactionId,
+def insertadminAdjustment(ResultSet rs, PreparedStatement adminAdjustmentStmt,
                    Long toTransactionId, Map preparedData, Connection conn) {
-    // Check for duplicates in the `transfer` table
-    def transferId = rs.getInt('transfer_id')
-    def duplicateCheckStmt = conn.prepareStatement('SELECT COUNT(*) FROM transfer WHERE transfer_id = ?')
-    duplicateCheckStmt.setInt(1, transferId)
+    // Check for duplicates in the `admin_adjustment` table
+    def adminAdjustmentId = rs.getInt('admin_adjustment_id')
+    def duplicateCheckStmt = conn.prepareStatement('SELECT COUNT(*) FROM admin_adjustment WHERE admin_adjustment_id = ?')
+    duplicateCheckStmt.setInt(1, adminAdjustmentId)
     def duplicateResult = duplicateCheckStmt.executeQuery()
     duplicateResult.next()
     def count = duplicateResult.getInt(1)
@@ -459,33 +414,23 @@ def insertTransfer(ResultSet rs, PreparedStatement transferStmt, Long fromTransa
     duplicateCheckStmt.close()
 
     if (count > 0) {
-        log.warn("Duplicate transfer detected with transfer_id: ${transferId}, skipping insertion.")
+        log.warn("Duplicate admin_adjustment detected with admin_adjustment_id: ${adminAdjustmentId}, skipping insertion.")
         return null
     }
 
     // Proceed with insertion if no duplicate exists
-    def categoryId = getTransferCategoryId(rs.getString('transfer_category'), preparedData)
     def statusId = getStatusId(rs.getString('current_status'), preparedData)
-    transferStmt.setInt(1, rs.getInt('from_organization_id'))
-    transferStmt.setInt(2, rs.getInt('to_organization_id'))
-    transferStmt.setObject(3, fromTransactionId)
-    transferStmt.setObject(4, toTransactionId)
-    transferStmt.setTimestamp(5, rs.getTimestamp('agreement_date'))
-    transferStmt.setTimestamp(6, rs.getTimestamp('transaction_effective_date'))
-    transferStmt.setBigDecimal(7, rs.getBigDecimal('price_per_unit'))
-    transferStmt.setInt(8, rs.getInt('quantity'))
-    transferStmt.setString(9, rs.getString('from_org_comment'))
-    transferStmt.setString(10, rs.getString('to_org_comment'))
-    transferStmt.setString(11, rs.getString('gov_comment'))
-    transferStmt.setObject(12, categoryId)
-    transferStmt.setObject(13, statusId)
-    transferStmt.setString(14, rs.getString('recommendation'))
-    transferStmt.setTimestamp(15, rs.getTimestamp('create_date'))
-    transferStmt.setTimestamp(16, rs.getTimestamp('update_date'))
-    transferStmt.setInt(17, rs.getInt('create_user'))
-    transferStmt.setInt(18, rs.getInt('update_user'))
-    transferStmt.setInt(19, rs.getInt('transfer_id'))
-    def result = transferStmt.executeQuery()
-    return result.next() ? result.getInt('transfer_id') : null
+    adminAdjustmentStmt.setInt(1, rs.getInt('to_organization_id'))
+    adminAdjustmentStmt.setObject(2, toTransactionId)
+    adminAdjustmentStmt.setTimestamp(3, rs.getTimestamp('transaction_effective_date'))
+    adminAdjustmentStmt.setInt(4, rs.getInt('compliance_units'))
+    adminAdjustmentStmt.setString(5, rs.getString('gov_comment'))
+    adminAdjustmentStmt.setObject(6, statusId)
+    adminAdjustmentStmt.setTimestamp(7, rs.getTimestamp('create_date'))
+    adminAdjustmentStmt.setTimestamp(8, rs.getTimestamp('update_date'))
+    adminAdjustmentStmt.setInt(9, rs.getInt('create_user'))
+    adminAdjustmentStmt.setInt(10, rs.getInt('update_user'))
+    adminAdjustmentStmt.setInt(11, rs.getInt('admin_adjustment_id'))
+    def result = adminAdjustmentStmt.executeQuery()
+    return result.next() ? result.getInt('admin_adjustment_id') : null
 }
-
