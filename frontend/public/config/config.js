d10b9fefc524399737d7ae05f37e5801e306a501
Daniel Haselhan <daniel.haselhan@quartech.com>
2024-12-19 10:01:30 -0800

    Code Review Feedback

--- frontend/public/config/config.js
+++ frontend/public/config/config.js
@@ -1,6 +1,6 @@
 export const config = {
   api_base: 'http://localhost:8000/api',
-  tfrs_base: 'http://localhost:3001',
+  tfrs_base: 'http://localhost:3000',
   keycloak: {
     REALM: 'standard',
     CLIENT_ID: 'low-carbon-fuel-standard-5147',
