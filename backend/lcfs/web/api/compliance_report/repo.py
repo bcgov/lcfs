2d05661154a12341d5e9596ac222e1958fcb7c6f
Arturo Reyes Lopez <arturo_reyes_lopez@hotmail.com>
2025-02-03 12:10:49 -0700

    Remove duplicates in response. Sort the response.

--- backend/lcfs/web/api/compliance_report/repo.py
+++ backend/lcfs/web/api/compliance_report/repo.py
@@ -409,7 +409,9 @@ class ComplianceReportRepository:
         subquery = subquery.subquery()
         # Join the main ComplianceReport table with the subquery to get the latest version per group
         query = (
-            select(ComplianceReport)
+            select(ComplianceReport,
+                   CompliancePeriod.compliance_period_id,
+                   ComplianceReport.update_date)
             .join(
                 subquery,
                 and_(
@@ -439,6 +441,11 @@ class ComplianceReportRepository:
                 joinedload(ComplianceReport.history)
                 .joinedload(ComplianceReportHistory.user_profile)
                 .joinedload(UserProfile.organization),
+            )
+            .distinct()
+            .order_by(
+                desc(CompliancePeriod.compliance_period_id),
+                desc(ComplianceReport.update_date)
             )
         )
 
