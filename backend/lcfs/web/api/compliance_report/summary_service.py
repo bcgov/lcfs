ddb0b9faf4f2525c528d0dff1acd57a27eb4e313
prv-proton <prashanth.venkateshappa@gov.bc.ca>
2025-08-22 09:10:47 -0700

    .

--- backend/lcfs/web/api/compliance_report/summary_service.py
+++ backend/lcfs/web/api/compliance_report/summary_service.py
@@ -642,27 +642,26 @@ class ComplianceReportSummaryService:
 
         fossil_quantities = self.repo.aggregate_quantities(all_fossil_records, True)
         # line 2
+        # Ensure non-Canadian renewable diesel volumes are not added into lines 1 or 2.
         filtered_renewable_fuel_supplies = [
             fs
             for fs in effective_fuel_supplies
-            if (fs.fuel_catgory.category != "Diesel" and fs.fuel_type.renewable)
-            # Ensure non-Canadian renewable diesel volumes are not added into lines 1 or 2.
-            or (
-                fs.fuel_category.category == "Diesel"
-                and fs.fuel_type.renewable
-                and (fs.is_canada_produced or fs.is_canada_imported)
+            if fs.fuel_type.renewable
+            and (
+                compliance_year < 2025
+                or fs.fuel_category.category != "Diesel"
+                or (fs.fuel_category.category == "Diesel" and fs.is_canada_produced)
             )
         ]
 
         filtered_renewable_other_uses = [
             ou
             for ou in effective_other_uses
-            if (ou.fuel_category.category != "Diesel" and ou.fuel_type.renewable)
-            # Ensure non-Canadian renewable diesel volumes are not added into lines 1 or 2.
-            or (
-                ou.fuel_category.category == "Diesel"
-                and ou.fuel_type.renewable
-                and (ou.is_canada_produced or ou.is_canada_imported)
+            if ou.fuel_type.renewable
+            and (
+                compliance_year < 2025
+                or ou.fuel_catgory.category != "Diesel"
+                or (ou.fuel_category.category == "Diesel" and ou.is_canada_produced)
             )
         ]
 
