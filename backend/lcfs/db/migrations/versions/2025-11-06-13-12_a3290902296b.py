326315435f8dc690e3682a9ba5e81b85c2b12c24
Alex Zorkin <alex@thecruxstudios.com>
2025-12-10 10:28:09 -0800

    fix: migration ordering

--- backend/lcfs/db/migrations/versions/2025-11-06-13-12_a3290902296b.py
+++ backend/lcfs/db/migrations/versions/2025-11-06-13-12_a3290902296b.py
@@ -1,7 +1,7 @@
 """add renewable diesel fuel type
 
 Revision ID: a3290902296b
-Revises: 5e1a2c3d4f67
+Revises: a7b8c9d0e1f2
 Create Date: 2025-04-11 13:29:03.149771
 
 """
@@ -10,7 +10,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "a3290902296b"
-down_revision = "5e1a2c3d4f67"
+down_revision = "a7b8c9d0e1f2"  # Updated: TFRS migrations come after main migrations
 branch_labels = None
 depends_on = None
 
