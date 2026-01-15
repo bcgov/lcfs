4cd5c1f8eca1420006109d8e0a8f31ff55630c71
Prashanth <Prashanth.venkateshappa@gov.bc.ca>
2026-01-15 06:19:54 -0800

    Fix: LCFS – Historical TFRS Bugs - Schedule B/Fuel Supply shows current TCI/EER/RCI instead of historical values

--- backend/lcfs/db/migrations/versions/2026-01-21-00-03_87592f5136b3.py
+++ backend/lcfs/db/migrations/versions/2026-01-21-00-03_87592f5136b3.py
@@ -30,27 +30,27 @@ def upgrade():
             create_date,
             update_date
         ) VALUES
-        -- 2013 Gasoline
+        -- 2013 Diesel
         (4, 2, 92.38, 0, true, NOW(), NOW()),
-        -- 2014 Gasoline
+        -- 2014 Diesel
         (5, 2, 92.38, 0, true, NOW(), NOW()),
-        -- 2015 Gasoline
+        -- 2015 Diesel
         (6, 2, 91.21, 0, true, NOW(), NOW()),
-        -- 2016 Gasoline
+        -- 2016 Diesel
         (7, 2, 90.28, 0, true, NOW(), NOW()),
-        -- 2017 Gasoline
+        -- 2017 Diesel
         (8, 2, 90.02, 0, true, NOW(), NOW()),
-        -- 2018 Gasoline
+        -- 2018 Diesel
         (9, 2, 88.60, 0, true, NOW(), NOW()),
-        -- 2019 Gasoline
+        -- 2019 Diesel
         (10, 2, 87.18, 0, true, NOW(), NOW()),
-        -- 2020 Gasoline
-        (11, 2, 85.28, 0, true, NOW(), NOW()),
-        -- 2021 Gasoline
+        -- 2020 Diesel
+        (11, 2, 86.15, 0, true, NOW(), NOW()),
+        -- 2021 Diesel
         (12, 2, 85.11, 0, true, NOW(), NOW()),
-        -- 2022 Gasoline
-        (13, 2, 84.00, 0, true, NOW(), NOW()),
-        -- 2023 Gasoline
+        -- 2022 Diesel
+        (13, 2, 84.04, 0, true, NOW(), NOW()),
+        -- 2023 Diesel
         (14, 2, 81.86, 0, true, NOW(), NOW());
     """
         )
@@ -69,27 +69,27 @@ def upgrade():
             create_date,
             update_date
         ) VALUES
-        -- 2013 Diesel
+        -- 2013 Gasoline
         (4, 1, 86.20, 0, true, NOW(), NOW()),
-        -- 2014 Diesel
+        -- 2014 Gasoline
         (5, 1, 86.20, 0, true, NOW(), NOW()),
-        -- 2015 Diesel
+        -- 2015 Gasoline
         (6, 1, 85.11, 0, true, NOW(), NOW()),
-        -- 2016 Diesel
+        -- 2016 Gasoline
         (7, 1, 84.23, 0, true, NOW(), NOW()),
-        -- 2017 Diesel
+        -- 2017 Gasoline
         (8, 1, 83.74, 0, true, NOW(), NOW()),
-        -- 2018 Diesel
+        -- 2018 Gasoline
         (9, 1, 82.41, 0, true, NOW(), NOW()),
-        -- 2019 Diesel
+        -- 2019 Gasoline
         (10, 1, 81.09, 0, true, NOW(), NOW()),
-        -- 2020 Diesel
-        (11, 1, 79.33, 0, true, NOW(), NOW()),
-        -- 2021 Diesel
+        -- 2020 Gasoline
+        (11, 1, 80.13, 0, true, NOW(), NOW()),
+        -- 2021 Gasoline
         (12, 1, 79.17, 0, true, NOW(), NOW()),
-        -- 2022 Diesel
-        (13, 1, 78.00, 0, true, NOW(), NOW()),
-        -- 2023 Diesel
+        -- 2022 Gasoline
+        (13, 1, 78.20, 0, true, NOW(), NOW()),
+        -- 2023 Gasoline
         (14, 1, 76.14, 0, true, NOW(), NOW());
     """
         )
