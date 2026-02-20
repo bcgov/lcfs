03ce64681638bafc884f36b6d2e1b9e7728dfdc0
Prashanth <Prashanth.venkateshappa@gov.bc.ca>
2026-02-20 09:06:43 -0800

    .

--- backend/lcfs/db/migrations/versions/2026-03-01-12-00_b1234567890c.py
+++ backend/lcfs/db/migrations/versions/2026-03-01-12-00_b1234567890c.py
@@ -1,7 +1,7 @@
 """Add active flag to compliance report charging equipment
 
 Revision ID: b1234567890c
-Revises: a1b2c3d4e5f9
+Revises: b6f4d2e9c1a0
 Create Date: 2026-03-01 12:00:00.000000
 """
 
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "b1234567890c"
-down_revision = "a1b2c3d4e5f9"
+down_revision = "b6f4d2e9c1a0"
 branch_labels = None
 depends_on = None
 
