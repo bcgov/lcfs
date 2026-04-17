97240bf9205d2ad1213d4a994b25558a2f1cf20c
Alex Zorkin <alex@thecruxstudios.com>
2026-04-17 12:53:02 -0700

    chore: migration order

--- backend/lcfs/db/migrations/versions/2026-04-08-12-00_b4e7f2a1c8d3.py
+++ backend/lcfs/db/migrations/versions/2026-04-08-12-00_b4e7f2a1c8d3.py
@@ -1,7 +1,7 @@
 """Update unit of measure label m³ to m³ (15°C and 1 atm)
 
 Revision ID: b4e7f2a1c8d3
-Revises: 098cf79762b9
+Revises: e0f9a4a6316a
 Create Date: 2026-04-08 12:00:00.000000
 """
 
@@ -9,7 +9,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "b4e7f2a1c8d3"
-down_revision = "098cf79762b9"
+down_revision = "e0f9a4a6316a"
 branch_labels = None
 depends_on = None
 
