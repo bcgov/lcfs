bd7818b8f56ab8f72fae2f72875bc1daec228527
Prashanth <Prashanth.venkateshappa@gov.bc.ca>
2026-07-08 09:17:47 -0700

    .

--- backend/lcfs/db/migrations/versions/2026-07-02-12-00_b7c8d9e0f1a2.py
+++ backend/lcfs/db/migrations/versions/2026-07-02-12-00_b7c8d9e0f1a2.py
@@ -1,7 +1,7 @@
 """Add credit market monthly report notification type.
 
 Revision ID: b7c8d9e0f1a2
-Revises: f2c8a9d1b3e5
+Revises: b1c2d3e4f5a6
 Create Date: 2026-07-02 12:00:00.000000
 """
 
@@ -9,7 +9,7 @@ from alembic import op
 
 
 revision = "b7c8d9e0f1a2"
-down_revision = "f2c8a9d1b3e5"
+down_revision = "b1c2d3e4f5a6"
 branch_labels = None
 depends_on = None
 
